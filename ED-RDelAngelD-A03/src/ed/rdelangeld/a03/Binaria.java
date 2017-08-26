94af5207b155a7172cc9b76f1634c6d4b03bc8bd
AngelDavila1997 <angel.davila17@hotmail.com>
2017-08-26 17:52:44 -0500

    Update Binaria.java

--- ED-RDelAngelD-A03/src/ed/rdelangeld/a03/Binaria.java
+++ ED-RDelAngelD-A03/src/ed/rdelangeld/a03/Binaria.java
@@ -53,17 +53,64 @@ public class Binaria extends BusquedaClase implements Ordenamientos{
                        vector[menor] = a; //el valor del vector se guarda en a
                }
        }}
-    
+
     @Override
     public void insercion() {
-         for(int i = 1; i < vector.length; i++){
-             int temp = vector[i];
-             int j = i - 1;
-                while((j >= 0) && (vector[j] > temp)){
+         for(int i = 1; i < vector.length; i++){//Mientras i sea menor al largo del vector
+             int temp = vector[i];//El valor del vector en la posicion i se guarda en temp
+             int j = i - 1;// En la variable j e guarda el valor de i -1
+                while((j >= 0) && (vector[j] > temp)){//Mientras que j sea mayor o igual a0
+                                                        //Y el valor del vector en la pos j sea mayor que temp
                     vector[j+1] = vector[j];
                     j--;
                 }
              vector[j+1] = temp;
          }
+    }
+
+    @Override
+    public void burbuja() {
+        for(int i = 1; i < vector.length ; i++){
+            for(int j = 0; j < vector.length-1; j++){
+                if (vector[j] > vector[j+1]){
+                    intercambio(j, j+1);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void quicksort(int inicio, int ultimo) {
+        int i, j, pivote;
+        i = inicio;
+        j = ultimo;
+        pivote = vector[inicio + (ultimo-inicio)/2];//Se obtiene el pivote sumando los valores de inicio y final-inicio
+                                                    //del vector entre 2 y el valor de esa posicion se le da a pivote
+        do{//Mientras que la condicion de i<=j se cumpla
+           while(vector[i] < pivote){//Si el valor del vector en la posicion i es menor al pivote
+               i++;//El apuntador avanzara
+           }
+           while(vector[j] > pivote){//Si el valor del vector en la posicion j es mayor al pivote
+               j--;//El apuntador ira para atras
+           }
+           if(i <= j){//Si i<=j se realizara el intercambio de valores y se moveran los apuntadores
+            intercambio(i, j);
+            i++;
+            j--;
+           }
+        }while(i <= j);
+        
+        if(inicio < j){//Si el inicio del arreglo es menor que j
+            quicksort(inicio, j); //Se enviara un arreglo con estas dos posiciones como parametros
+        }
+        if(i < ultimo){//Si el i es menor que el final del arreglo
+            quicksort(i, ultimo);//Se enviara un arreglo con estas dos posiciones como parametros
+        }
+    }
+    
+    public void intercambio(int i, int j){
+        int temp = vector[i];
+        vector[i] = vector[j];
+        vector[j] = temp;    
     }
     }
