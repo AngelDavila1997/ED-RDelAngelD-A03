d2fb32ce3fc9bae51825e9afe79edb7ac1bf4086
AngelDavila1997 <angel.davila17@hotmail.com>
2017-08-26 17:51:31 -0500

    Update EDRDelAngelDA03.java

--- ED-RDelAngelD-A03/src/ed/rdelangeld/a03/EDRDelAngelDA03.java
+++ ED-RDelAngelD-A03/src/ed/rdelangeld/a03/EDRDelAngelDA03.java
@@ -23,7 +23,7 @@ public class EDRDelAngelDA03 {
         System.out.println("-------------------------");
         
         Binaria b = new Binaria ();
-        int[] a = {18, 17, 16, 19, 21, 15, 4};
+        int[] a = {98, 17, 16, 5, 21, 15, 4};
         b.setVector(a);
         b.showVector();
         b.seleccion();
@@ -33,12 +33,32 @@ public class EDRDelAngelDA03 {
         System.out.println("-------------------------");
         
         Binaria c = new Binaria();
-        int[] d = {18, 28, 16, 79, 21, 35, 4};
+        int[] d = {39, 17, 78, 5, 15, 10, 7};
         c.setVector(d);
         c.showVector();
         c.insercion();
         c.showVector();
-        c.showF(c.fetch(35));
+        c.showF(c.fetch(17));
+        
+        System.out.println("-------------------------");
+        
+        Binaria e = new Binaria();
+        int[] f = {8, 7, 16, 14, 101, 13, 6};
+        e.setVector(f);
+        e.showVector();
+        e.burbuja();
+        e.showVector();
+        e.showF(e.fetch(16));
+        
+        System.out.println("-------------------------");
+        
+        Binaria h = new Binaria();
+        int[] g = {178, 35, 6, 17, 21, 19, 22};
+        h.setVector(g);
+        h.showVector();
+        h.quicksort(0, a.length-1);
+        h.showVector();
+        h.showF(h.fetch(21));
     }
     
 }
